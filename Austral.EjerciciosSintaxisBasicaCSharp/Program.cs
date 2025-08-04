1e62944eefe451198ed857b8647bacc154902392
PaulinaB8 <paulinabruno343@gmail.com>
2025-08-04 11:54:37 -0300

    Ejecución de los ejercicios: Variables y tipos, Operadores y Estructuras de control

--- Austral.EjerciciosSintaxisBasicaCSharp/Program.cs
+++ Austral.EjerciciosSintaxisBasicaCSharp/Program.cs
@@ -3,12 +3,19 @@
 // =====================
 
 // a) Declarar una variable con el nombre de una empresa y mostrarla por consola.
+string nombreEmpresa = "Samsung";
 
 // b) Declarar una variable con la cantidad de empleados y mostrarla por consola.
 
+int cantidadEmpleados = 400;
+
 // c) Declarar una variable decimal con el ingreso mensual estimado.
 
+double ingresoMensual = 1000000.589;
+
 // d) Mostrar por consola un mensaje que diga el nombre de la empresa, cantidad e empleados e ingreso.
+
+Console.WriteLine($"La empresa {nombreEmpresa} cuenta con {cantidadEmpleados} de empleados, y tiene ingresos mensuales de {ingresoMensual}");
 
 
 // ==============
@@ -17,13 +24,29 @@
 
 // a) Declarar dos precios de productos y calcular el total de los dos con IVA.
 
+int precioNetoProductoUno = 150;
+int precioNetoProductoDos = 200;
+double precioTotalProductoUno = precioNetoProductoUno + (precioNetoProductoUno * 0.21);
+double precioTotalProductoDos = precioNetoProductoDos + (precioNetoProductoDos * 0.21);
+Console.WriteLine($"Precio total del producto uno = {precioTotalProductoUno}; Precio total del producto dos = {precioTotalProductoDos}");
+
 // b) En base a la siguiente variable -> int ingresoAnual = 1500;
 //    Hacer otra variable que represente el ingreso mensual y mostrarla por consola.
 
+int ingresoAnual = 1500;
+int ingresoMensualDos = 1500 / 12;
+Console.WriteLine($"Su ingreso mensual es {ingresoMensual}");
+
 // c) Crear una variable que represente si el ingreso anual es mayor a 1000 o no.
+
+bool ingresoMayorMil = ingresoAnual > 1000;
+Console.WriteLine($"Su ingreso anual es mayor a 1000 = {ingresoMayorMil}");
 
 // d) Calcular cuánto gana una empresa por mes si su ingreso anual es 1500
 //    y tiene que pagar 12% de impuestos al estado.
+
+double ingresoMensualNeto = (ingresoAnual - (ingresoAnual * 0.12)) / 12;
+Console.WriteLine($"El ingreso mensual de Samsung descontado los impuestos del estado son {ingresoMensualNeto}");
 
 
 // ==========================
@@ -35,20 +58,101 @@
 //     - Si tiene Entre 11 y 50 → "Pyme"
 //     - Si tiene más de 50 → "Big"
 
+int numeroEmpleados = 60;
+
+if (numeroEmpleados <= 10)
+{
+    Console.WriteLine("Startup");
+}
+else if (numeroEmpleados<50 && numeroEmpleados > 11)
+{
+    Console.WriteLine("Pyme");
+}
+else
+{
+    Console.WriteLine("Big");
+}
+
 // b) En base al monto de inversión de una empresa, clasificar la inversión como:
 //     - Si es menor a 10,000 → "Baja"
 //     - Si está entre 10,000 y 100,000 → "Media"
 //     - Si es mayor a 100,000 → "Alta"
 
+int montoInversion = 110000;
+
+if (montoInversion < 10000)
+{
+    Console.WriteLine($"La inversión fue baja");
+}
+else if (montoInversion >= 10000 && montoInversion <= 100000)
+{
+    Console.WriteLine($"La inversión fue media");
+}
+else
+{
+    Console.WriteLine($"La inversión fue alta");
+}
+
 // c) Según el tipo de cliente ("mayorista" o "minorista"), mostrar el precio final de un producto con descuento si corresponde:
 //     - Si es mayorista → 20% de descuento
-//     - Si es minorista → precio completo
+//     - Si es minorista → 50% precio completo
+//     - Si es cliente final -> 80% descuento 
+
+// Opción con if 
+
+string tipoCliente = "minorista";
+int precioProducto = 150;
+
+if (tipoCliente == "mayorista")
+{
+    Console.WriteLine(precioProducto - (precioProducto * 0.2));
+}
+else
+{
+    Console.WriteLine(precioProducto);
+}
+
+//Opción con switch
+
+switch (tipoCliente)
+{
+    case "mayorista":
+        Console.WriteLine("Tipo de cliente: mayorista. Se aplica 20% de descuento");
+        Console.WriteLine(precioProducto - (precioProducto * 0.2));
+        break;
+
+    case "minorista":
+        Console.WriteLine("Tipo de cliente: minorista. Se aplica 50% de descuento");
+        Console.WriteLine(precioProducto - (precioProducto * 0.5));
+        break;
+
+    case "cliente final":
+        Console.WriteLine("Tipo de cliente: cliente final. Se aplica 80% de descuento");
+        Console.WriteLine(precioProducto - (precioProducto * 0.8));
+        break;
+
+    default: Console.WriteLine("Cliente no encontrado"); break;
+}
+
 
 // d) Una distribuidora tiene 20 unidades de cerveza en stock. Un cliente solicita 25 unidades.
 //    Verificar si hay suficiente stock para concretar la venta:
 //     - Si alcanza, mostrar por consola: "Venta confirmada. Cervezas en stock suficientes."
 //     - Si no alcanza, mostrar: "No hay suficientes cervezas en stock. Faltan X unidades."
 //       (donde X debe calcularse dinámicamente en base a las variables)
+
+int stockCerveza = 20;
+int solicitudCerveza = 25;
+
+if (stockCerveza >= solicitudCerveza)
+{
+    Console.WriteLine("Venta confirmada. Cervaezas en stock suficiente.");
+}
+else
+{
+    int faltanCervezas = solicitudCerveza - stockCerveza;
+    Console.WriteLine($"No hay suficientes cervaezas en stock. Faltan {faltanCervezas} unidades");
+}
 
 
 // ==========
